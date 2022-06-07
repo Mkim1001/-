78145c6ec16f6e7e8a0a34c677917d947ed0f4f3
Mkim1001 <54555662+Mkim1001@users.noreply.github.com>
2022-06-07 16:59:16 -0400

    Add files via upload
    오늘 파이썬을 복습했당 천천히 공부해서 멋진 컴공이 되어야지

---
+++ 파이썬_복습_6:7.py
@@ -0,0 +1,80 @@
+print ('Hello, World!')
+
+1 + 1 //일더하기 일은 귀요미
+
+1 - 2 //값이 -로 나옴
+
+2 * 2 //4 곱하기
+
+5 / 2 //나누기
+
+int (5/2) //나눈수를 정수로 바꿈
+
+float(1+2) //소숫점을 찍음
+
+x = 10  //변수 만들기 변수이름 = 값
+
+x + 1  //변수에 1 을 더함
+
+x  //10 소환
+
+del x  // x 값을 지움
+
+a, b, c = 1, 2, 3  //변수들에 값을 넣음
+a + b  //1=2
+a  //1
+b  //2
+c  //3
+
+
+x = int(input(15)) //정수로 바꾸는듯
+x
+
+v= float(input(2)) //정수를 소수로바꿈
+v 
+
+a,b = input('a').split('b') //이건 좀 햇갈림
+a
+b
+a,b = map (int, input(2).split(3)) //map 잘 모르겠음
+
+a= 5
+b= 25
+
+a % b 
+print ("""Hello, world!  ///뛰여쓰기를 하는 스트링은 뛰어쓰는 문장에 수만틈 "를 넣어줘야하는듯
+안녕하세요.
+Python입니다.""")
+
+list = [100, 10, 5]  //리스트를 만듬
+
+print(list)  //리스트를 불러옴
+list[0]  //리스트에서 가장 첫번째 값을 불러옴
+list[1]   //2번째 값가지고오기
+list[2]
+list[] = 1000 //리스트에 새로운 값을 넣기
+list[3] //방금 넣은 값을 불러옴
+
+range(6) //0에서부터 5까지 의 값을 나열하는 듯
+range(0, 6) //0부더 5까지 나열함
+range(0, 6, 2) //0부더 5까지 2개식 더해서 값을불러옴
+
+\\tuples are array of sum, however you cannot edit or rearrange the sum. It's read only. 
+sum of tuples can be used in , to make it as indivisuals but you ()is not nesesessary when makeing tuples \\
+//tuples can be named anything nott "tuples"//
+
+tuple = (10, 10.5, 90)
+tuple = 10, 10.5, 90
+print(tuple)
+tuple[0]
+minjun = (1998, 2, 26, 21, 23) 
+print (sum(minjun))
+minjun[0]
+minjun[1]
+minjun[2]
+minjun[3]
+
+minjuni1 = (100, )
+//sequence types//
+
+//오늘 배운것, 값을 불러올때에는 0이 1번째이다 이유는 컴터가 숫자를 0,1,2,3,4,5...로 세기때문
